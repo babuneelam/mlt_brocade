ec05db278a011465cd0cb2f3bf80fccb1c0f9e2b
babuneelam <babuneelam@users.noreply.github.com>
2016-03-07 06:15:21 -0800

    Update mlt.h

--- mlt.h
+++ mlt.h
@@ -1,6 +1,8 @@
 
 #ifdef CONFIG_SILKWORM_MLT
 
+#include "mlt_common.h"
+
 //#define MLT_STRESS_TEST
 //#define MAX_MLT_HASH_NODES 20000 
 #ifndef MLT_STRESS_TEST
@@ -9,10 +11,8 @@
 #define MLT_MAX_HASH 16
 #endif
 
-#define MAX_MLT_STK_DEPTH 30
 #define MAX_MLT_WRP_STK_DEPTH 20
 #define MAX_MLT_HASH_STK_DEPTH 25
-#define MLT_STK_TRACE_SKIP_CNT 2
 #define MAX_MLT_FUNC_NAME_LEN 300
 #define MLT_MAGIC_NUM 0x73914BA3
 #define MAX_MLT_DISPLAY_NODES 100
@@ -43,6 +43,10 @@ enum {
 	MLT_NO_STK_TRACE,
 	MLT_ALLOC_FAILED,
 	MLT_UNINITIALIZED,
+#ifdef MLT_DEBUG
+        MLT_NODE_REUSED,
+        MLT_NODE_FREED,
+#endif
 	MLT_MAX_STAT,
 };
 
@@ -55,6 +59,10 @@ char MLT_stats_names[MLT_MAX_STAT + 1][100] = {
 	"MLT: No Stack Trace error \0",
 	"MLT: hash node allocation failed\0",
 	"MLT: uninitialized\0",
+#ifdef MLT_DEBUG
+        "MLT: Node re-used (indicates better leverage for performance)\0",
+        "MLT: Node freed\0",
+#endif
 	"Max Stat\0"
 };
 
@@ -63,21 +71,13 @@ typedef struct MLT_hash_node {
 	struct list_head MLT_hash_list_next;
 	unsigned long stk_trace[MAX_MLT_HASH_STK_DEPTH];	/* kmalloc function trace *//*TBD: change unsigned long to kernel's typedefs to ensure 64-bit compliance */
 	unsigned int stk_fn_hash;
-	struct {
-#define MLT_LOCK_BIT_BE 31	/*  the compiler lays out the bit fields as big endian */
-		unsigned long bit_lock:1;
-		unsigned long delete_pending:1;
-		unsigned long delete_wait_count:6;
-		unsigned long hash_index:16;
-		unsigned long chain_len:8;
-	} hash_control;
 	atomic_t kmalloc_cnt;	/* count to represent the number of
 				   kmallocs from this function trace */
-	unsigned long size;	/* allocation size */
 	atomic_t total_alloc_size;	/* allocation size */
-	unsigned short hash_count;
 	unsigned short stk_trace_len;
-	struct list_head scheduled_for_deletion;
+#ifdef MLT_DEBUG
+        unsigned short chain_len;
+#endif
 } MLT_hash_node_t;
 
 /* MLT hash node data structure */
@@ -96,105 +96,5 @@ typedef struct {
 	unsigned long vp;
 	MLT_hash_node_t *p_stats;
 } MLT_vp_node_t;
-
-/*Function delcarations */
-static __always_inline unsigned int mlt_get_stack_trace(unsigned long *mlt_stk_trace);
-static __always_inline int mlt_validate_sp(unsigned long sp, struct task_struct *p,
-                       unsigned long nbytes);
-#ifdef CONFIG_IRQSTACKS
-static __always_inline int mlt_valid_irq_stack(unsigned long sp, struct task_struct *p,
-                                  unsigned long nbytes);
-#endif
-
-
-static __always_inline uint32_t bob_jenkins_hash(uint32_t a)
-{
-	a = (a + 0x7ed55d16) + (a << 12);
-	a = (a ^ 0xc761c23c) ^ (a >> 19);
-	a = (a + 0x165667b1) + (a << 5);
-	a = (a + 0xd3a2646c) ^ (a << 9);
-	a = (a + 0xfd7046c5) + (a << 3);
-	a = (a ^ 0xb55a4f09) ^ (a >> 16);
-	return a;
-}
-
-static __always_inline uint32_t JSHash(char *str, unsigned int len)
-{
-	unsigned int hash = 1315423911;
-
-	for (; len; len--) {
-		hash ^= ((hash << 5) + (*str++) + (hash >> 2));
-	}
-
-	return hash;
-}
-
-#ifdef CONFIG_IRQSTACKS
-static __always_inline int mlt_valid_irq_stack(unsigned long sp, struct task_struct *p,
-                                  unsigned long nbytes)
-{
-        unsigned long stack_page;
-        unsigned long cpu = task_cpu(p);
-
-        /*
-         * Avoid crashing if the stack has overflowed and corrupted
-         * task_cpu(p), which is in the thread_info struct.
-         */
-        if (cpu < NR_CPUS && cpu_possible(cpu)) {
-                stack_page = (unsigned long) hardirq_ctx[cpu];
-                if (sp >= stack_page + sizeof(struct thread_struct)
-                    && sp <= stack_page + THREAD_SIZE - nbytes)
-                        return 1;
-
-                stack_page = (unsigned long) softirq_ctx[cpu];
-                if (sp >= stack_page + sizeof(struct thread_struct)
-                    && sp <= stack_page + THREAD_SIZE - nbytes)
-                        return 1;
-        }
-        return 0;
-}
-
-#else
-#define mlt_valid_irq_stack(sp, p, nb)      0
-#endif /* CONFIG_IRQSTACKS */
-
-static __always_inline int mlt_validate_sp(unsigned long sp, struct task_struct *p,
-                       unsigned long nbytes)
-{
-        unsigned long stack_page = (unsigned long)task_stack_page(p);
-
-        if (sp >= (unsigned long)end_of_stack(p)
-            && sp <= stack_page + THREAD_SIZE - nbytes)
-                return 1;
-
-        return mlt_valid_irq_stack(sp, p, nbytes);
-}
-
-static __always_inline unsigned int mlt_get_stack_trace(unsigned long *mlt_stk_trace)
-{
-        unsigned long sp;
-        unsigned int i;
-
-        asm("mr %0,1" : "=r" (sp));
-
-        for (i=1; i< MLT_STK_TRACE_SKIP_CNT; i++)
-        {
-                if (!validate_sp(sp, current, STACK_FRAME_OVERHEAD))
-                        return 0;
-                sp = ((unsigned long *)sp)[0];
-        }
-
-        for (i=0; i< MAX_MLT_STK_DEPTH; i++)
-        {
-                if (!mlt_validate_sp(sp, current, STACK_FRAME_OVERHEAD))
-                        return i;
-
-                mlt_stk_trace[i] = ((unsigned long *)sp)[STACK_FRAME_LR_SAVE];
-
-                sp = ((unsigned long *)sp)[0];
-        }
-
-        return MAX_MLT_STK_DEPTH;
-}
 
 #endif
